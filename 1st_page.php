29f9c96879ef8f8ae25eb95eaa64645814d0f9d7
Shouty <anisahmed50@gmail.com>
2023-10-21 04:24:10 +0600

    Add .gitattributes to specify binary for .css and .js files

---
+++ 1st_page.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Charupath</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="css/style.css" rel="stylesheet"/>
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+  </head>
+  <body>
+  
+  
+		<div class="full_page">
+			<div class="logo">
+				<img src="images/logo.jpg" alt="Grid-01" />
+			</div>
+			<div class="without_logo">
+				<div class="menu_bar">
+					<div class="btn"><a href="login.php"><button>Login</button></a></div>
+					<div class="btn"><a href="registration.php"><button>Registration</button></a></div>
+					<div class="btn"><a href="aboutus.php"><button>About Us</button></a></div>
+					<div class="btn"><a href="contactus.php"><button>Contact Us</button></a></div>
+					<div class="btn"><a href="branches.php"><button>Branches</button></a></div>
+					<div class="btn"><a href="courses.php"><button>Courses</button></a></div>
+				
+				</div>
+				
+				<div >
+					<img class="image_part" src="images/first_page.jpg" alt="Grid-01" />
+				</div>
+				
+				
+			</div>
+		</div>
+		
+    
+    <script src="js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Charupath</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="css/style.css" rel="stylesheet"/>
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+  </head>
+  <body>
+  
+  
+		<div class="full_page">
+			<h1>LOGIN</h1>
+		</div>
+		
+    
+    <script src="js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
